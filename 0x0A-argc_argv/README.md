45d40dedfeeea4dd82ee95bd3e20164289481bbd
Tawo-123 <tawochawira@gmail.com>
2023-10-06 10:14:34 -0700

    Pro

---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+Readme Argc and Argv
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdbool.h>
+
+int _putchar(char c);
+bool is_positive_number(const char *str);
+int calculate_minimum_coins(int argc, char *argv[]);
+
+#endif /* MAIN_H */
