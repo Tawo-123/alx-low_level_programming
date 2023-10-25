a95f6821296bbb74bb5ed8dc894c0132d705eda9
Tawo-123 <tawochawira@gmail.com>
2023-10-25 07:22:13 -0700

    Function

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,23 +1,21 @@
-#include <stddef.h>
 #include "function_pointers.h"
-#include <stdlib.h>
-
+#include <stdio.h>
 /**
- * array_iterator - Executes a function on each element of an array
- * @array: Pointer to the array of integers
- * @size: Size of the array
- * @action: Pointer to the function to be executed on each element
+ * array_iterator - prints each array elem on a newl
+ * @array: array
+ * @size: how many elem to print
+ * @action: pointer to print in regular or hex
+ * Return: void
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	if (array != NULL && action != NULL)
+	unsigned int i;
+
+	if (array == NULL || action == NULL)
+		return;
+
+	for (i = 0; i < size; i++)
 	{
-		size_t i;
-
-		for (i = 0; i < size; ++i)
-		{
-			action(array[i]);
-		}
+		action(array[i]);
 	}
-
 }
--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,28 +1,22 @@
-#include <stddef.h>
 #include "function_pointers.h"
-
 /**
- * int_index - Searches for an integer in an array
- * @array: Pointer to the array of integers
- * @size: Number of elements in the array
- * @cmp: Pointer to the function used to compare values
- *
- * Return: Index of the first element for which the cmp function does not
- * return 0. If no element matches, return -1. If size <= 0, return -1.
+ * int_index - return index place if comparison = true, else -1
+ * @array: array
+ * @size: size of elements in array
+ * @cmp: pointer to func of one of the 3 in main
+ * Return: 0
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	if (array != NULL && cmp != NULL && size > 0)
-	{
-		int i;
+	int i;
 
-		for (i = 0; i < size; ++i)
-		{
-			if (cmp(array[i]) != 0)
-			{
-				return (i);
-			}
-		}
+	if (array == NULL || size <= 0 || cmp == NULL)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+			return (i);
 	}
 	return (-1);
 }
