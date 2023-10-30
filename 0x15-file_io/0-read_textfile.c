7c7a9d80381974eae64c1f22606f3cf030505da7
Tawo-123 <tawochawira@gmail.com>
2023-10-30 12:38:42 -0700

    VINI

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,26 +1,28 @@
-#include <stdio.h>
+#include "main.h"
 #include <stdlib.h>
 
-void read_textfile(const char *filename, size_t letters)
+/**
+ * read_textfile- Read text file print to STDOUT.
+ * @filename: text file being read
+ * @letters: number of letters to be read
+ * Return: w- actual number of bytes read and printed
+ *        0 when function fails or filename is NULL.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
 {
-    char *buf;
-    ssize_t fd;
-    ssize_t w;
-    ssize_t t;
+	char *buf;
+	ssize_t fd;
+	ssize_t w;
+	ssize_t t;
 
-    fd = open(filename, O_RDONLY);
-    if (fd == -1)
-        return;
-    buf = malloc(sizeof(char) * letters);
-    t = read(fd, buf, letters);
-    w = write(STDOUT_FILENO, buf, t);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	t = read(fd, buf, letters);
+	w = write(STDOUT_FILENO, buf, t);
 
-    free(buf);
-    close(fd);
-}
-
-int main(void)
-{
-    read_textfile("filename.txt", 1024);
-    return (0);
+	free(buf);
+	close(fd);
+	return (w);
 }
